61eaa68b2b9dbdf0dd63aaaa8245ee61aa6e2a0f
Angel Aguayo <a.aguayo30@gmail.com>
2018-01-13 02:46:42 -0700

    Let the connecting begin

--- app/src/main/java/com/example/aagua/hackaz2018/MainActivity.java
+++ app/src/main/java/com/example/aagua/hackaz2018/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.aagua.hackaz2018;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    private Button child;
+    private Button parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,26 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        //ActionBar actionBar= getActionBar();
-        //actionBar.hide();
+
+        child = findViewById(R.id.child);
+        child.setOnClickListener(this);
+        parent = findViewById(R.id.parent);
+        parent.setOnClickListener(MainActivity.this);
+
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.child:
+                Intent childRegister=new Intent(MainActivity.this, CalendarActivity.class);
+
+                startActivity(childRegister);
+                break;
+            case R.id.parent:
+                break;
+            default:
+                break;
+        }
+
     }
 }
