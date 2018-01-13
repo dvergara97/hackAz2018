b7d0167ee12a6348b41d845e8bcf1d9d57bf4fb0
Angel Aguayo <a.aguayo30@gmail.com>
2018-01-13 15:18:05 -0700

    Log in petty much almost done

---
+++ app/src/main/java/com/example/aagua/hackaz2018/AdminActivity.java
@@ -0,0 +1,29 @@
+package com.example.aagua.hackaz2018;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by aagua on 1/13/2018.
+ */
+
+public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button eventButton;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        eventButton=findViewById(R.id.eventButton);
+        eventButton.setOnClickListener(this);
+    }
+
+
+    public void OnClick(View view){
+        Intent eventIntent=new Intent(this, inputEventNameAndDescription.class);
+        startActivity(eventIntent);
+    }
+}
--- app/src/main/java/com/example/aagua/hackaz2018/SignUpActivity.java
+++ app/src/main/java/com/example/aagua/hackaz2018/SignUpActivity.java
@@ -1,22 +1,85 @@
 package com.example.aagua.hackaz2018;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by aagua on 1/13/2018.
  */
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    Toolbar myToolbar;
+    Button signButton;
+    EditText firstText, lastText, phoneText, emailText, pswdText,conPswdText;
+    CheckBox isParent;
+
+    private DatabaseReference DBReference;
+
     public void onCreate(Bundle SavedInstanceState){
         super.onCreate(SavedInstanceState);
 
         setContentView(R.layout.activity_signup);
+
+        myToolbar = findViewById(R.id.signin_toolbar);
+        myToolbar.setTitle("Sign Up");
+        setSupportActionBar(myToolbar);
+
+        signButton=findViewById(R.id.signupButton);
+        signButton.setOnClickListener(this);
+
+        firstText = findViewById(R.id.firstName);
+        lastText=findViewById(R.id.lastName);
+        phoneText=findViewById(R.id.phoneText);
+        emailText=findViewById(R.id.email);
+        pswdText=findViewById(R.id.pswdText);
+        conPswdText=findViewById(R.id.conPswdText);
+        isParent=findViewById(R.id.isParent);
+        DBReference= FirebaseDatabase.getInstance().getReference();
     }
 
-    public void onClick(View views){
 
+    public void onClick(View view){
+        String firstName=firstText.getText().toString().trim();
+        String lastName=lastText.getText().toString().trim();
+        String phoneNumber=phoneText.getText().toString().trim();
+        String email=emailText.getText().toString().trim();
+        String password=pswdText.getText().toString().trim();
+        boolean isAdmin=isParent.isChecked();
+
+        String emailPattern="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (email.matches(emailPattern)) {
+            User person=new User(firstName,lastName, phoneNumber, email,
+                    password,isAdmin);
+
+            DBReference.child("users").child(firstName+" "+lastName).setValue(person);
+
+            goToNextScreen(person);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void goToNextScreen(User person){
+        if(person.isAdmin()){
+            Intent personIntent=new Intent(this, AdminActivity.class);
+            startActivity(personIntent);
+        }
+        else{
+            Intent personIntent=new Intent(this, CalendarActivity.class);
+            startActivity(personIntent);
+        }
     }
 }
