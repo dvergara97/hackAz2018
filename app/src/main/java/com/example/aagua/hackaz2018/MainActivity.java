bc9059390104f67faed7d4941ffdd671053670a6
Angel Aguayo <a.aguayo30@gmail.com>
2018-01-13 03:41:03 -0700

    Basic Login screens made (both parent and child connect to the same for now)

--- app/src/main/java/com/example/aagua/hackaz2018/MainActivity.java
+++ app/src/main/java/com/example/aagua/hackaz2018/MainActivity.java
@@ -10,8 +10,8 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener{
-    private Button child;
-    private Button parent;
+    private Button childButton;
+    private Button parentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,27 +19,37 @@ public class MainActivity extends Activity implements View.OnClickListener{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        child = findViewById(R.id.child);
-        child.setOnClickListener(this);
-        parent = findViewById(R.id.parent);
-        parent.setOnClickListener(MainActivity.this);
+        childButton = findViewById(R.id.child);
+        childButton.setOnClickListener(this);
+        parentButton = findViewById(R.id.parent);
+        parentButton.setOnClickListener(MainActivity.this);
 
     }
 
     public void onClick(View view){
         switch (view.getId()){
             case R.id.child:
-                Intent childRegister=new Intent(MainActivity.this, CalendarActivity.class);
-
-                startActivity(childRegister);
+                switchToChildLogin();
                 break;
             case R.id.parent:
+                switchToAdultLogin();
                 break;
             default:
                 break;
         }
 
+    }
+
+    public void switchToChildLogin(){
+        Intent childRegister=new Intent(MainActivity.this, ChildLogIn.class);
+        startActivity(childRegister);
+    }
+
+    public void switchToAdultLogin(){
+        Intent parentRegister=new Intent(this, ChildLogIn.class);
+        startActivity(parentRegister);
     }
 }
